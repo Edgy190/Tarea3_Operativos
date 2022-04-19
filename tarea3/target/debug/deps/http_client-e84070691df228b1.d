a9b5253c2c967694bf5e3bd7ba43a62db239e167
Renzo Barra <66889568+Edgy190@users.noreply.github.com>
2022-04-18 19:09:01 -0600

    Add files via upload
    Servidor, sigue siendo single threaded. Tengo que ver como hacer el front-end con python y trabajar en el html para recibir el archivo binario

---
+++ tarea3/target/debug/deps/http_client-e84070691df228b1.d
@@ -0,0 +1,5 @@
+/home/renzo/Desktop/tarea3/target/debug/deps/http_client-e84070691df228b1: src/http_client.rs
+
+/home/renzo/Desktop/tarea3/target/debug/deps/http_client-e84070691df228b1.d: src/http_client.rs
+
+src/http_client.rs:
---
+++ tarea3/target/debug/http_client.d
@@ -0,0 +1,1 @@
+/home/renzo/Desktop/tarea3/target/debug/http_client: /home/renzo/Desktop/tarea3/src/http_client.rs
